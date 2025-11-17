8dd1f41d296bda071878bc6191a0b3af21fc7470
IBMCP4BADevelopment <87987290+ganeshdemo@users.noreply.github.com>
2025-11-17 20:54:52 +0300

    Add files via upload

---
+++ ICN Plugin/bin/com/ganeshdemo/cp4ba/translate/WebContent/TranslatePlugin.js
@@ -0,0 +1,11 @@
+require(["dojo/_base/declare",
+         "dojo/_base/lang","dojo/aspect"], 
+function(declare, lang , aspect) {		
+	
+	/*aspect.after(icm.pgwidget.inbasket.dijit.InbasketContentPane.prototype, "postCreate", function(response){
+		var openItems = JSON.stringify(icmglobal._openItems);
+			
+	});*/
+
+	
+});
